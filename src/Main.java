cfaa98952231bdb94ee0c470a3f581e6bc75e001
Carlos Reiakvam <carlosreiakvam@gmail.com>
2021-01-21 12:40:34 +0100

    New branch, attempting the nearly impossible

--- src/Main.java
+++ src/Main.java
@@ -6,8 +6,14 @@ public class Main {
 
     static ArrayList<Fruit> fruitArrayList = new ArrayList<>();
     static Scanner input = new Scanner(System.in);
+    private static ObjectInputStream inputStream;
+    private static ObjectOutputStream outputStream;
 
     public static void main(String[] args) throws IOException, ClassNotFoundException {
+        // Initialize streams
+        initStreams();
+
+        // Menu
         while (true) {
             System.out.println("1: read, 2 write, 3 exit");
             int userChoice = input.nextInt();
@@ -23,16 +29,22 @@ public class Main {
                     fruitArrayList.add(new Fruit(typeOfFruit));
                     writeToFile(new Fruit(typeOfFruit));
                 }
-                case 3 -> System.exit(0);
+                case 3 -> {
+                    System.exit(0);
+                    inputStream.close();
+                }
                 default -> System.out.println("wrong input");
             }
         }
 
     }
 
+    public static void initStreams() throws IOException {
+        outputStream = new ObjectOutputStream(new FileOutputStream("fruits.txt"));
+        inputStream = new ObjectInputStream(new FileInputStream("fruits.txt"));
+    }
 
     public static void writeToFile(Fruit fruit) throws IOException {
-        ObjectOutputStream outputStream = new ObjectOutputStream(new FileOutputStream("fruits.txt"));
         outputStream.writeObject(fruitArrayList);
         outputStream.close();
     }
@@ -40,9 +52,13 @@ public class Main {
     // TODO fix: Read from non existing file results in NullPointerException
     // TODO fix: Read from existing file only reads one object
     public static void readFromFile() throws ClassNotFoundException, IOException {
-        ObjectInputStream inputStream = new ObjectInputStream(new FileInputStream("fruits.txt"));
-        fruitArrayList = (ArrayList<Fruit>) inputStream.readObject();
-        inputStream.close();
+        try {
+            while (true)
+                fruitArrayList.add((Fruit) inputStream.readObject());
+        } catch (EOFException e) {
+            System.out.println("end of file detected");
+        }
+
     }
 
 /*
